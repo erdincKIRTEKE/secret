e1a84ec1808f7c30cef38e9ea7c4d719707a8dab
erdinckirteke <erdinckirteke@gmail.com>
2023-07-31 21:18:02 +0300

    hash version

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ import express from 'express';
 import ejs from 'ejs';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import encrypt from 'mongoose-encryption'
+import bcrypt, { hash } from 'bcrypt'
+import session from 'express-session';
+
+
+const saltRounds=10;  
+
 
 
 
@@ -29,7 +34,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields: ['password']});
+
 
 
 const User =new mongoose.model('user',userSchema);
@@ -47,31 +52,79 @@ app.get('/register',function(req,res){
     res.render('register.ejs');
 })
 
-app.post('/register',function(req,res){
-    const user =new User({email:req.body.username,password:req.body.password})
-    user.save();
-    res.render('secrets')
-})
 
-app.post('/login', async function(req,res){
-    try{
+
+app.post('/register',async function(req,res,){
+
+var salt = crypto.randomBytes(16);
+crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+  if (err) { return next(err); }
+  db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    req.body.username,
+    hashedPassword,
+    salt
+  ], function(err) {
+    if (err) { return next(err); }
+    var user = {
+      id: this.lastID,
+      username: req.body.username
+    };
+    req.login(user, function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  });
+});
+});
+
+
+// app.post('/register',async function(req,res){
+//     try{
+//         bcrypt.hash(req.body.password,saltRounds,async function(err,hash){
+//             const user =new User({email:req.body.username,password:hash})
+//             const newUSer=await user.save();
+//             if(!newUSer){
+//                 throw Error(' user  is not registered')
+//             }
+//             if(err){
+//             throw Error('check your mail and password')
+//             }else{
+//                 res.render('secrets')
+//             }
+   
+//         })
+       
+//     }catch(err){
+//         console.error(err);
+//         res.send(err.message)
+
+//     }
+   
+// })
+
+app.post('/login',function(req,res){
         const username=req.body.username;
-        const password=(req.body.password)
-       const foundUser=await User.findOne({email:username});
+        const password=req.body.password
+    
+       async function checkUser(username, password) {
 
-    if(foundUser){
-        if(foundUser.password===password){
-            res.render('secrets')
-        }else{
-            throw Error('Please check your password')
-        }
-    }else throw Error('Please check your email.')
-    }catch(err){
-        res.send(err.message)
+        try{//... fetch user from a db etc.
+            const user=await User.findOne({email:username});
+            if (!user){ res.send('check your email')}
+    
+            const match = await bcrypt.compare(password, user.password);
+    
+            if(match) {
+               res.render('secrets')
+            } else throw Error('Please check your password')    //.
+
+        }catch(err){
+            res.send(err.message)
     }
 
-
-})
+    }
+    checkUser(username,password)
+    })
 
 
 
